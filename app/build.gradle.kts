3850dbc248698e17a220a2901051f347cd447da7
PaperMacheKen <papermacheken@gmail.com>
2026-04-12 22:01:53 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.icebreakerpmken"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
